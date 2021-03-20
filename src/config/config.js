59a4f351e46436d911fc855ec032183c35d5b08f
Mak Yen Wei <shaonmak@gmail.com>
2021-03-20 13:26:13 +0800

    fix: mongodbUrl disabled

--- src/config/config.js
+++ src/config/config.js
@@ -8,7 +8,7 @@ const envVarsSchema = Joi.object()
   .keys({
     NODE_ENV: Joi.string().valid('production', 'development', 'test').required(),
     PORT: Joi.number().default(3000),
-    MONGODB_URL: Joi.string().required().description('Mongo DB url'),
+    /* MONGODB_URL: Joi.string().required().description('Mongo DB url'), */
     JWT_SECRET: Joi.string().required().description('JWT secret key'),
     JWT_ACCESS_EXPIRATION_MINUTES: Joi.number().default(30).description('minutes after which access tokens expire'),
     JWT_REFRESH_EXPIRATION_DAYS: Joi.number().default(30).description('days after which refresh tokens expire'),
