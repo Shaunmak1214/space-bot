3a3eb1c4b6c7106bc4bd5db4c1ddcb75ba8bd202
Mak Yen Wei <shaonmak@gmail.com>
2021-03-20 13:38:16 +0800

    feat: disabled passport

--- src/app.js
+++ src/app.js
@@ -4,11 +4,11 @@ const xss = require('xss-clean');
 const mongoSanitize = require('express-mongo-sanitize');
 const compression = require('compression');
 const cors = require('cors');
-const passport = require('passport');
+/* const passport = require('passport'); */
 const httpStatus = require('http-status');
 const config = require('./config/config');
 const morgan = require('./config/morgan');
-const { jwtStrategy } = require('./config/passport');
+/* const { jwtStrategy } = require('./config/passport'); */
 const { authLimiter } = require('./middlewares/rateLimiter');
 const routes = require('./routes/v1');
 const { errorConverter, errorHandler } = require('./middlewares/error');
@@ -42,8 +42,8 @@ app.use(cors());
 app.options('*', cors());
 
 // jwt authentication
-app.use(passport.initialize());
-passport.use('jwt', jwtStrategy);
+/* app.use(passport.initialize());
+passport.use('jwt', jwtStrategy); */
 
 // limit repeated failed requests to auth endpoints
 if (config.env === 'production') {
